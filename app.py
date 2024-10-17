4060598a27a58f73da87010547787fb54e6800fd
byte-seif <datadrivenaipro@gmail.com>
2024-10-17 23:11:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def run():
     model = joblib.load(open('model.joblib','rb'))
 
     st.title("Lab 10.2, Sentiment Analysis Using Streamlit")
-    st.text("Don't try to confuse me!.")
+    st.text("DON'T TRY TO CONFUSE ME, I'M STILL LEARNING!.")
     st.text("")
     userinput = st.text_input('Enter text below, then click the Predict button.', placeholder='Input text HERE')
     st.text("")
